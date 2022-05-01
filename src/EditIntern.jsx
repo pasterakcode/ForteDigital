708b29eff10055682823b2e709709a64837ebc7f
pasterakcode <pasterakcode@gmail.com>
2022-05-02 00:12:23 +0200

    edit component EditIntern and add styles

--- src/EditIntern.jsx
+++ src/EditIntern.jsx
@@ -7,20 +7,80 @@ import EditForm from "./components/EditForm/EditForm";
 
 const EditIntern = () => {
   const { id } = useParams();
-  const [intern, setIntern] = useState([]);
+  console.log(id);
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    internshipStart: '',
+    internshipEnd: '',
+  });
 
+  const inputs = [
+    {
+      id: 1,
+      name: "name",
+      type: 'text',
+      label: 'Full name *',
+      errorValidationInfo: 'Enter a valid name!',
+      pattern: "([a-zA-Z',.-]+( [a-zA-Z',.-]+)*){2,30}",
+      required: true,
+      rowWidth: '100%'
+    },
+    {
+      id: 2,
+      name: "email",
+      type: 'email',
+      label: 'Email address *',
+      errorValidationInfo: 'Enter a valid email address!',
+      required: true,
+      rowWidth: '100%'
+    },
+    {
+      id: 3,
+      name: "internshipStart",
+      type: 'date',
+      label: 'Internship start *',
+      errorValidationInfo: 'Start date can not be empty!',
+      required: true,
+      rowWidth: '50%',
+      max: values.internshipEnd
+    },
+    {
+      id: 4,
+      name: "internshipEnd",
+      type: 'date',
+      label: 'Internship end *',
+      errorValidationInfo: 'End date can not be empty!',
+      required: true,
+      rowWidth: '50%',
+      min: values.internshipStart
+    }
+  ]
+
+  const handleChangeInputValue = e => {
+    setValues({ ...values, [e.target.name]: e.target.value })
+  }
+  const handleSubmit = e => {
+    console.log('click submit');
+    e.preventDefault();
+  }
   useEffect(() => {
     const fetchIntern = async () => {
         const response = await fetch(`http://localhost:3001/interns/${id}`);
         const intern = await response.json();
-        setIntern(intern);
+  
+        setValues({
+          name: intern.name,
+          email: intern.email,
+          internshipStart: intern.internshipStart.split('').slice(0,10).join(''),
+          internshipEnd: intern.internshipEnd.split('').slice(0,10).join('')
+        });
       };
       fetchIntern();
   }, [id]);
 
-
+  console.log(values);
   return (
-    <>
       <div className={styles.participantEdit}>
         <NavLink className={styles.linkToBack} to={`/`}>
           <Arrow />
@@ -29,9 +89,13 @@ const EditIntern = () => {
         <div className={styles.header}>
           <h1 className={styles.header__content}>Edit</h1>
         </div>
-        <EditForm intern={intern}/>
+        <form onSubmit={handleSubmit}>
+        {inputs.map(input => (
+          <EditForm key={input.id} {...input} value={values[input.name]} handleChangeInputValue={handleChangeInputValue}/>
+        ))}
+        <button type="submit">Submit</button>
+        </form>
       </div>
-    </>
   );
 };
 
--- src/components/EditForm/EditForm.jsx
+++ src/components/EditForm/EditForm.jsx
@@ -2,63 +2,17 @@ import React, { useEffect, useState } from "react";
 import styles from "./EditForm.module.css";
 // import { ReactComponent as Calendar } from './assets/arrow.svg';
 
-const EditForm = ({ intern }) => {
-  const [inputNameValue, setInputNameValue] = useState("");
-  const [inputEmailValue, setInputEmailValue] = useState("");
-  
-  let defaultDate = new Date()
-  defaultDate.setDate(defaultDate.getDate() + 3);
-
-  let startDate = new Date()
-  startDate.setDate(startDate.getDate());
-
-  const [inputInternshipStart, setInputInternshipStart] = useState(startDate)
-
- 
-  useEffect(() => {
-    if (intern) {
-      setInputNameValue(intern.name);
-      setInputEmailValue(intern.email);
-    }
-  }, [intern]);
-
+const EditForm = ( { id, label, errorValidationInfo, handleChangeInputValue, rowWidth, ...inputProps } ) => {
+  const [focused, setFocused] = useState(false);
+  const inputWidth = {
+    width: rowWidth,
+  }
   return (
-    <form>
-      <div className={styles.form__row}>
-        <label htmlFor="name">Full name *</label>
-        <input
-          type="text"
-          name="name"
-          value={inputNameValue}
-          onChange={(e) => setInputNameValue(e.target.value)}
-        />
-      </div>
-      <div className={styles.form__row}>
-        <label htmlFor="email">Email address *</label>
-        <input
-          type="text"
-          name="email"
-          value={inputEmailValue}
-          onChange={(e) => setInputEmailValue(e.target.value)}
-        />
-      </div>
-      <div className={styles.form__row}>
-        <div className={styles.form__calendar}>
-          <label htmlFor="internshipStart">Internship start *</label>
-          <input
-            type="date"
-            name="internshipStart"
-            value={inputInternshipStart.toLocaleDateString("en-CA")}
-            onChange={e => setInputInternshipStart(new Date(e.target.value))}
-            />
-        </div>
-        <div className={styles.form__calendar}>
-          <label htmlFor="internshipEnd">Internship end *</label>
-          <input type="date" name="internshipEnd" />
-        </div>
-      </div>
-      <input type="submit" value="Submit" />
-    </form>
+   <div className={styles.inputArea} style={inputWidth}>
+     <label>{label}</label>
+     <input {...inputProps} onChange={handleChangeInputValue} onBlur={() => setFocused(true)} focused={focused.toString()}/>
+     <span className={styles.errorValidationInfo}>{errorValidationInfo}</span>
+   </div>
   );
 };
 
